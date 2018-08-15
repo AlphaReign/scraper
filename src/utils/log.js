4d0948cf4385e75fd524a886c3456b7ef5cf8932
William <william@prefinem.com>
2018-08-15 15:46:47 -0500

    log fn should be simple

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,29 +1,9 @@
-// import fs from 'fs';
-
-const logData = [];
-
-// const persist = () => fs.writeFileSync('./logs.txt', logData.map((record) => record.logMessage).join(`\n`));
-
-export const log = (message, output = true) => {
+export const log = (message) => {
 	const date = new Date();
 	const time = `[${`0${date.getHours()}`.slice(-2)}:${`0${date.getMinutes()}`.slice(-2)}:${`0${date.getSeconds()}`.slice(-2)}]`;
-	const logMessage = message && message.message ? `${time}: ${message.message}` : `${time}: ${message}`;
+	const output = message && message.message ? `${time}: ${message.message}` : `${time}: ${message}`;
 
-	logData.push({
-		date,
-		error: message && message.message ? message : undefined,
-		logMessage,
-		message,
-	});
-
-	// persist();
-
-	if (output) {
-		console.log(logMessage);
-		if (message.message) {
-			console.log(message);
-		}
-	}
+	console.log(output);
 };
 
-export default log;
+export default { log };
